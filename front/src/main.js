a08bde069ffa576f7402a9a517b2d623efb739c0
hpipou <mr.aoudjit@gmail.com>
2022-01-19 10:46:37 +0100

    Changer l'icon de la page de vues avec celle de l entreprise groupamania

--- front/src/main.js
+++ front/src/main.js
@@ -1,6 +1,45 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './axios'
 import router from './router'
-import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faImage } from '@fortawesome/free-solid-svg-icons'
+import { faPen } from '@fortawesome/free-solid-svg-icons'
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
+import {faCommentAlt } from '@fortawesome/free-solid-svg-icons'
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons'
+import { faCogs } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 
-createApp(App).use(store).use(router).mount('#app')
+
+
+library.add(faSignOutAlt);
+library.add(faUser);
+library.add(faImage);
+library.add(faPen);
+library.add(faArrowDown);
+library.add(faArrowUp);
+library.add(faCommentAlt);
+library.add(faPlusCircle);
+library.add(faTrashAlt);
+library.add(faEllipsisV);
+library.add(faCogs);
+library.add(faBars);
+library.add(faHome);
+
+
+
+
+
+
+
+
+createApp(App).use(router).mount('#app')
